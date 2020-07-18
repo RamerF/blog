89aba53d48301e7f6fab482c964733f844d2deab
ramer <1390635973@qq.com>
2020-07-19 02:16:22 +0800

    添加: 文章,文章标签.

--- src/main/java/io/github/ramerf/blog/BlogApplication.java
+++ src/main/java/io/github/ramerf/blog/BlogApplication.java
@@ -3,6 +3,8 @@ package io.github.ramerf.blog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 /**
  * The type App controller.
@@ -11,8 +13,13 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
  */
 @EntityScan({"io.github.ramerf.blog.entity.domain", "io.github.ramerf.blog.system.entity.domain"})
 @SpringBootApplication
-public class AppController {
+public class BlogApplication extends SpringBootServletInitializer {
   public static void main(String[] args) {
-    SpringApplication.run(AppController.class, args);
+    SpringApplication.run(BlogApplication.class, args);
+  }
+
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+    return builder.sources(BlogApplication.class);
   }
 }

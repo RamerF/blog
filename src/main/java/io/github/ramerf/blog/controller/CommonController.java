4910e64b73f07b77bb32fb263d41313f0f2b2150
ramer <1390635973@qq.com>
2020-08-10 01:41:37 +0800

    添加: 首页基本完成.

--- src/main/java/io/github/ramerf/blog/controller/CommonController.java
+++ src/main/java/io/github/ramerf/blog/controller/CommonController.java
@@ -1,14 +1,16 @@
 package io.github.ramerf.blog.controller;
 
+import io.github.ramerf.blog.entity.pojo.ArticlePoJo;
 import io.github.ramerf.blog.service.ArticleService;
+import io.github.ramerf.wind.core.condition.SortColumn;
+import io.github.ramerf.wind.core.condition.SortColumn.Order;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import java.util.Map;
 import javax.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * The type Common controller.
@@ -24,8 +26,11 @@ public class CommonController {
 
   @GetMapping
   @ApiOperation("首页")
-  public String index(Map<String, Object> map) {
-    map.put("articles", articleService.list(null));
+  public String index(
+      @RequestParam(value = "page", defaultValue = "1") int page, Map<String, Object> map) {
+    map.put(
+        "articles",
+        articleService.list(null, page, 1, SortColumn.by(ArticlePoJo::getCreateTime, Order.DESC)));
     return "user/index";
   }
 }
